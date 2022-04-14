a7df01d8ebec48ddfbad102b0097364f6d2d3f34
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-04-14 08:24:24 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# YourOnboardingKit
+
+A description of this package.
---
+++ Tests/YourOnboardingKitTests/YourOnboardingKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import YourOnboardingKit
+
+final class YourOnboardingKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(YourOnboardingKit().text, "Hello, World!")
+    }
+}
