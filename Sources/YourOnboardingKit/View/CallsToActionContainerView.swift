18e1f1525febe2c0742526bdb58783fd87a4c6dd
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-04-15 13:43:19 +0100

    add call to action buttons to the `CallsToActionContainerView`

--- Sources/YourOnboardingKit/View/CallsToActionContainerView.swift
+++ Sources/YourOnboardingKit/View/CallsToActionContainerView.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class CallsToActionContainerView: UIView {
 
+    // MARK: - Notifiers
+    var nextButtonTapped: (() -> Void)?
+    var getStartedButtonTapped: (() -> Void)?
+
     // MARK: - Properties
     private let viewTintColor: UIColor
 
@@ -18,8 +22,11 @@ class CallsToActionContainerView: UIView {
         button.layer.borderColor = viewTintColor.cgColor
         button.layer.borderWidth = 2
         button.setTitleColor(viewTintColor, for: .normal)
-        button.titleLabel?.font = UIFont(name: "Helvetica neue", size: 16)
-        button.layer.cornerRadius = 12
+        button.titleLabel?.font = UIFont(name: "Helvetica neue", size: 18)
+        button.layer.cornerRadius = 10
+        button.addTarget(self,
+                         action: #selector(didTapNextButton),
+                         for: .touchUpInside)
         return button
     }()
 
@@ -28,14 +35,28 @@ class CallsToActionContainerView: UIView {
         button.setTitle(CallToAction.getStarted.description, for: .normal)
         button.layer.borderColor = viewTintColor.cgColor
         button.setTitleColor(.white, for: .normal)
-        button.titleLabel?.font = UIFont(name: "Helvetica neue", size: 16)
+        button.titleLabel?.font = UIFont(name: "Helvetica neue", size: 18)
         button.backgroundColor = viewTintColor
-        button.layer.cornerRadius = 12
+        button.layer.cornerRadius = 10
         button.layer.shadowColor = viewTintColor.cgColor
         button.layer.shadowOpacity = 0.5
         button.layer.shadowOffset = .init(width: 4, height: 4)
         button.layer.shadowRadius = 8
+        button.addTarget(self,
+                         action: #selector(didTapGetStartedButton),
+                         for: .touchUpInside)
         return button
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(
+            arrangedSubviews: [
+                nextButton,
+                getStarteButton
+            ])
+        stackView.axis = .horizontal
+        stackView.spacing = 24
+        return stackView
     }()
 
     // MARK: - Initializers
@@ -51,6 +72,32 @@ class CallsToActionContainerView: UIView {
 
     // MARK: - Object Behaviours
     private func layout() {
-        backgroundColor = .systemPink
+        backgroundColor = .white
+        addSubview(stackView)
+
+        stackView.snp.makeConstraints { make in
+            make.edges
+                .equalTo(self)
+                .inset(
+                    UIEdgeInsets(
+                    top: 20,
+                    left: 20,
+                    bottom: 20,
+                    right: 20))
+        }
+
+        nextButton.snp.makeConstraints { make in
+            make.width
+                .equalTo(getStarteButton.snp.width)
+                .multipliedBy(0.5)
+        }
+    }
+
+    @objc private func didTapNextButton(_ sender: UIButton) {
+        nextButtonTapped?()
+    }
+
+    @objc private func didTapGetStartedButton(_ sender: UIButton) {
+        getStartedButtonTapped?()
     }
 }
