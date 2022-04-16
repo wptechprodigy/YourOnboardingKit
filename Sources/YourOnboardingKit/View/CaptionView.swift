3bc93de7ebc4a88a83fbbfbec118abf61a01228d
Waheed Afolabi <waheed.freelancer16@gmail.com>
2022-04-17 00:32:09 +0100

    add space holders for the composing part of the `transitionView`

---
+++ Sources/YourOnboardingKit/View/CaptionView.swift
@@ -0,0 +1,21 @@
+//
+//  CaptionView.swift
+//  
+//
+//  Created by waheedCodes on 15/04/2022.
+//
+
+import UIKit
+
+class CaptionView: UIView {
+
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemPink
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is not initialized yet")
+    }
+}
--- Sources/YourOnboardingKit/View/TransitionView.swift
+++ Sources/YourOnboardingKit/View/TransitionView.swift
@@ -9,24 +9,79 @@ import UIKit
 
 class TransitionView: UIView {
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        layout()
+    // MARK: - Number of slides
+    private let slides: [Slide]
+    private let viewTintColor: UIColor
+
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .systemTeal
+        return imageView
+    }()
+
+    private lazy var barViews: [AnimatedBarView] = {
+        var barViews: [AnimatedBarView] = []
+        slides.forEach { _ in
+            barViews.append(AnimatedBarView())
+        }
+        return barViews
+    }()
+
+    private lazy var barStackView: UIStackView = {
+        let stackView = UIStackView()
+        barViews.forEach { barView in
+            stackView.addArrangedSubview(barView)
+        }
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+
+    private lazy var captionView: CaptionView = {
+        let captionView = CaptionView()
+        return captionView
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [imageView, captionView])
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        return stackView
+    }()
+
+    // MARK: - Initializers
+    init(slides: [Slide], tintColor: UIColor) {
+        self.slides = slides
+        self.viewTintColor = tintColor
+        super.init(frame: .zero)
+        layoutViews()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func layout() {
-        backgroundColor = .systemGreen
-    }
+    private func layoutViews() {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+        addSubview(stackView)
+        addSubview(barStackView)
+
+        stackView.snp.makeConstraints { make in
+            make.edges.equalTo(self)
+        }
+
+        barStackView.snp.makeConstraints { make in
+            make.leading.equalTo(snp.leading).offset(20)
+            make.trailing.equalTo(snp.trailing).offset(-20)
+            make.top.equalTo(snp.topMargin)
+            make.height.equalTo(4)
+        }
+
+        imageView.snp.makeConstraints { make in
+            make.height.equalTo(stackView.snp.height).multipliedBy(0.8)
+        }
     }
-    */
 }
